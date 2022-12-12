4dfb6cd2d835f262a01eae47c7ebd091b0bf1cc2
Chands <chands1432@gmail.com>
2022-12-12 16:42:34 +0530

    Post controller added
    Signed-off-by: Chands <chands1432@gmail.com>

--- README.md
+++ README.md
@@ -25,8 +25,44 @@ use socializedb;
 ├───controllers       # functions to connect routes to db operations
 ├───db                # db connection and model definitions
 ├───public            # html/css/js files for static part of the site
-└───routes            # express middlewares (route wise)
+├───routes            # express middlewares (route wise)
+└───utils             # Generate Random username
+
 ```
+
+## Business logic
+
+### Users
+
+1. **create users**
+   this will create a new user with a random username
+
+### Posts
+
+1. **create posts**
+   this will create a new post, required fields are
+
+   - username (the author of that post)
+   - title (the title of the post)
+   - body
+
+2. **show all posts**
+   list all existing posts, we should have following filtering support
+
+   - filter by username
+   - filter by query contained in title (search by title)
+
+3. **edit posts** `TBD`
+
+4. **delete posts** `TBD`
+
+### Comments
+
+1. **add a comment**
+
+2. **show all comments (under a post)**
+
+3. **show all comments (of a user)**
 
 ```shell
 git init
---
+++ src/controllers/posts.js
@@ -0,0 +1,73 @@
+const { Posts, Users } = require("../db/models");
+
+/**
+ * Create new post by a user with the given userId
+ * @param {number} userId
+ * @param {Text} title
+ * @param {Text} body
+ */
+async function createNewPost(userId, title, body) {
+  const post = await Posts.create({
+    title,
+    body,
+    userId,
+  });
+
+  return post;
+}
+
+/**
+ * Show all posts:
+ * showAllPosts({username: ''})
+ * showAllPosts({title: ''})
+ *
+ */
+async function showAllPosts(query) {
+  const posts = await Posts.findAll({
+    // where: {
+    //    ...query // ...query ToDo: handle query params
+    // }
+    include: [Users], // ...include works only when relations exists between models
+  });
+  return posts;
+}
+
+module.exports = {
+    createNewPost,
+    showAllPosts
+}
+
+/* Test post */
+/*
+async function test() {
+  //create new post
+
+  // console.log(
+  //     await createNewPost(
+  //         1,
+  //         'Sample Post',
+  //         'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor in'
+  //     )
+  // ),
+  // console.log(
+  //     await createNewPost(
+  //         2,
+  //         'Another Sample post',
+  //         'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor in'
+  //     )
+  // )
+
+  //fetch posts
+
+  const posts = await showAllPosts();
+  for (let p of posts) {
+    console.log(`
+        title: ${p.title}
+        body: ${p.body}
+        Author: ${p.user.name}
+        ============================`);
+  }
+}
+
+test();
+*/
