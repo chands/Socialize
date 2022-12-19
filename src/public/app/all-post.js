db41b42d84e00b0a260982bd5c4f298ddff02d65
Chands <chands1432@gmail.com>
2022-12-19 15:17:14 +0530

    re organised all-post js inside all-posts html
    Signed-off-by: Chands <chands1432@gmail.com>

---
+++ src/public/app/all-post.js
@@ -0,0 +1,25 @@
+function loadPosts() {
+	$.get('/api/posts', (posts) => {
+		for (let post of posts) {
+			$('#posts-container').append(
+				$(`
+			<div class="col-4">
+			<div class="card m-2">
+			<div class="card-body">
+			<h5 class="card-title">${post.title}</h5>
+			<h6 class="card-subtitle mb-2 text-muted">${post.user.name}</h6>
+			<p class="card-text">
+				${post.body.substring(0, 200)}
+				<a href="#" class="text-muted">...see more</a>
+			</p>
+			<a href="#" class="card-link">Like</a>
+			<a href="#" class="card-link">Comment</a>
+			<a href="#" class="card-link">Share</a>
+			</div>
+			</div>
+			</div>
+			`)
+			);
+		}
+	});
+}
